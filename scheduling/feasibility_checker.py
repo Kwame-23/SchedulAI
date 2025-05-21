bf8d892e6b65040567bd03087ea83fecb70ae48c
Malottey1 <123462271+Malottey1@users.noreply.github.com>
2025-05-21 09:14:27 +0000

    Update feasibility_checker.py

--- scheduling/feasibility_checker.py
+++ scheduling/feasibility_checker.py
@@ -50,7 +50,7 @@ def get_db_connection():
         conn = mysql.connector.connect(
             host='localhost',
             user='root',
-            password='Naakey057@',
+            password=' ',
             database='schedulai'
         )
         return conn
